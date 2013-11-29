f49d76829ffa05e8fe4d74eefeeb026da5a4b09d
Dmitry Komkov <dkomkov@dhcp-10-30-16-83.sw.ru>
2013-11-29 21:01:22 +0400

    helpers: improve class decorator with class name

--- lib/helpers.py
+++ lib/helpers.py
@@ -5,13 +5,15 @@ class DecoMeta(type):
 	def __new__(cls, name, bases, attrs):
 		for attr_name, attr_value in attrs.iteritems():
 			if isinstance(attr_value, types.FunctionType):
-				attrs[attr_name] = cls.deco(attr_value)
+				attrs[attr_name] = cls.deco(name, attr_value)
 		return super(DecoMeta, cls).__new__(cls, name, bases, attrs)
 
 	@classmethod
-	def deco(cls, func):
+	def deco(cls, name, func):
+		class_name = name
+		func_name = func.__name__
 		def wrapper(*args, **kwargs):
-			print "%s..." % func.__name__
+			print "# %s:%s ..." % (class_name, func_name)
 			func(*args, **kwargs)
 		return wrapper
 
