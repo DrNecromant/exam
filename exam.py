3f0a39b5aa07baf68194a8f339a792202b4b19e6
Dmitry <drnecromant@gmail.com>
2013-12-08 15:04:23 +0400

    exam: rename debug mode to fake mode
    debug is about additional logging
    but fake just reject all changes

--- exam.py
+++ exam.py
@@ -5,8 +5,8 @@ from _exam_lib import Exam
 parser = OptionParser()
 parser.add_option("--rus", "-r", action="store_true",
 	dest="rus", help="exam rus words")
-parser.add_option("--debug", "-d", action="store_true",
-	dest="debug", help="dry run")
+parser.add_option("--fake", "-f", action="store_true",
+	dest="fake", help="dry run")
 parser.add_option("--count", "-c", dest="count",
 	default = 20, type = "int", help="number of word to exam")
 
