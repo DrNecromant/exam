b1e20061c2842578d443380206fa0ea97b42af91
Dmitry <drnecromant@gmail.com>
2013-12-12 22:01:02 +0400

    xls: tune test files
    set width for columns

--- lib/xls.py
+++ lib/xls.py
@@ -7,14 +7,20 @@ class XLS():
 	def __init__(self):
 		pass
 
-	def dumpData(self, dst, words):
+	def dumpData(self, dst, words, column_num = 2):
 		wb = Workbook()
 		ws = wb.add_sheet("test")
 		row = 0
+		max_lens = [0] * column_num
 		for w in words:
-			ws.write(row, 0, w[0]) 
-			ws.write(row, 1, w[1]) 
+			for i in range (column_num):
+				l = len(w[i])
+				if l > max_lens[i]:
+					max_lens[i] = l
+				ws.write(row, i, w[i]) 
 			row += 1
+		for i in range (column_num):
+			ws.col(i).width = max_lens[i] * 256
 		wb.save(dst)
 
 	def loadData(self, filename, column_num = 2):
