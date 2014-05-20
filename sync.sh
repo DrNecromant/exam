1a072857d1ea44f74aefe5b056910574e74670af
Dmitry Komkov <drnecromant@gmail.com>
2014-05-21 00:41:35 +0400

    sync: correct shell script

--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-for i in {1..1000}; do python sync.py; t=$[ ( $RANDOM % 300 ) + 300 ]s; echo sleep $t; sleep $t; done
+for i in {1..1000}; do python sync.py -t 10; t=$[ ( $RANDOM % 600 ) + 300 ]s; echo sleep $t; sleep $t; done
